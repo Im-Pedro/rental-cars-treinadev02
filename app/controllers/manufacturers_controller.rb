45441af12ffef244af3ca95862eb3feb83858a29
TreinaDev 03 <treinadev03@Campuss-Mac-mini-2.local>
2019-11-29 17:36:26 -0300

    Admin cadastrou um fabricante

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -2,8 +2,22 @@ class ManufacturersController < ApplicationController
   def index
     @manufacturers = Manufacturer.all
   end
+
   def show
     id = params[:id]
-    @manufacturer = Manufacturer.id
+    @manufacturer = Manufacturer.find(id)
+  end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.create(manufacturer_params)
+
+    redirect_to @manufacturer
+  end
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_180434) do
+ActiveRecord::Schema.define(version: 2019_11_29_194639) do
 
   create_table "manufacturers", force: :cascade do |t|
     t.string "name"
